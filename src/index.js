1f0f9169e9ac6ee287557ec9e80ecde14835a642
Vladislav Khvostov <vladkhvo@gmail.com>
2017-11-11 16:07:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const koaResponseDispatcher = async (ctx, options) => {
 	try {
 		ctx.body = await options.body;
-        options.logger && options.logger(options.logLabel).log(ctx, options.succesLogMessage); 
+        	options.logger && options.logger(options.logLabel).log(ctx, options.succesLogMessage); 
 	} catch (error) {	
-        ctx.throw(500, options.errorStatusDescription)
-        options.logger && options.logger(options.logLabel).error(ctx, options.errorLogMessage, error); 
+        	ctx.throw(500, options.errorStatusDescription)
+        	options.logger && options.logger(options.logLabel).error(ctx, options.errorLogMessage, error); 
 	}
 };
 
